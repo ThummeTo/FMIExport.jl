0d47117c2ebabc0ac2269c27df3a241c940622d8
TT <tobias.thummerer@informatik.uni-augsburg.de>
2022-02-18 16:37:51 +0100

    bug fix

---
+++ test/src/Test.jl
@@ -0,0 +1,6 @@
+#
+# Copyright (c) 2021 Tobias Thummerer, Lars Mikelsons
+# Licensed under the MIT license. See LICENSE file in the project root for details.
+#
+
+# this is a dummy file to be a working package
