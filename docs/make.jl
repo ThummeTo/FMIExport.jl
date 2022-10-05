1515bda8879c1f385abe820c87815a1ee4a3fa1c
Johannes Stoljar <johannes.stoljar@uni-a.de>
2022-10-05 21:02:54 +0200

    Fix typo in docu make file

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@
 # Licensed under the MIT license. See LICENSE file in the project root for details.
 #
 
-using Documenter, FMI
+using Documenter, FMIExport
 using Documenter: GitHubActions
 
 makedocs(sitename="FMIExport.jl",
