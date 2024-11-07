043d2bf84b820d4ef66dac31e3028f717da37eed
Simon Exner <43469235+0815Creeper@users.noreply.github.com>
2024-11-07 12:51:39 +0100

    fixes for schedtask

--- test/bouncing_ball/bouncing_ball.jl
+++ test/bouncing_ball/bouncing_ball.jl
@@ -19,21 +19,39 @@ fsize = filesize(fmu_save_path)/1024/1024
 # Simulate FMU in Python / FMPy
 lockfile=joinpath(pwd(), "bouncing_ball", "lockfile.txt")
 logfile=joinpath(pwd(), "bouncing_ball", "FMPy-log.txt")
-
 t_start = "0.0"
 t_stop = "5.0"
 juliatestflag = "JULIA_@test:"
 #fmu_save_path = joinpath(pwd(), "BouncingBall.fmu")
-script_file = joinpath(pwd(), "bouncing_ball", "fmpy-bouncing_ball.py")
-try
-    rm(lockfile)
-catch e
-    println(e)
+# as commandline interface for task sheduling in windows does only allow 261 characters for \TR option, we need an external config file
+config_file = joinpath(pwd(), "bouncing_ball", "fmpy-bouncing_ball.config")
+open(config_file, "w+") do io
+    #line 1: lockfile
+    write(io, lockfile)
+    write(io, "\n")
+    #line 2: logfile
+    write(io, logfile)
+    write(io, "\n")
+    #line 3: fmu_save_path
+    write(io, fmu_save_path)
+    write(io, "\n")
+    #line 4: juliatestflag
+    write(io, juliatestflag)
+    write(io, "\n")
+    #line 5: t_start
+    write(io, t_start)
+    write(io, "\n")
+    #line 5: t_stop
+    write(io, t_stop)
+    write(io, "\n")
 end
-try
+
+script_file = joinpath(pwd(), "bouncing_ball", "fmpy-bouncing_ball.py")
+if isfile(lockfile)
+    rm(lockfile)
+end
+if isfile(logfile)
     rm(logfile)
-catch e
-    println(e)
 end
 
 `python -m pip install FMPy`
@@ -41,14 +59,17 @@ end
 using Dates
 tasktime = now() + Second(120)
 time = Dates.format(tasktime, "HH:MM")
-println(readchomp(`SCHTASKS /CREATE /SC ONCE /TN "ExternalFMIExportTesting\\BouncingBall-FMPy" /TR "python $script_file --t_start=$t_start --t_stop=$t_stop --fmufile=$fmu_save_path --logfile=$logfile --lockfile=$lockfile --juliatestflag=$juliatestflag" /ST $time`))
+println("FMPY-DEBUG-Flag")
+println(readchomp(`SCHTASKS /CREATE /SC ONCE /TN "ExternalFMIExportTesting\\BouncingBall-FMPy" /TR "python $script_file $config_file" /ST $time`))
 sleep(150)
+rm(config_file)
 time_wait_max = datetime2unix(now()) + 600.0
 if isfile(lockfile) || isfile(logfile)
     while isfile(lockfile) && datetime2unix(now()) < time_wait_max
         sleep(10)
     end
-    println("wating for FMPy-Task ended; FMPy-Task done: " * String(!isfile(lockfile)))
+    println("wating for FMPy-Task ended; FMPy-Task done: " * string(!isfile(lockfile)))
+    println(readchomp(`SCHTASKS /DELETE /TN ExternalFMIExportTesting\\BouncingBall-FMPy /f`))
     if !isfile(logfile)
         println("No log of FMPy-Task found")
         @test false # error: no log by fmpy created
@@ -64,9 +85,10 @@ if isfile(lockfile) || isfile(logfile)
     end
 else
     println("Error in FMPy-testsetup: Windows task scheduler did not start FMPy successfully or FMPy terminated prematurely before generating lock or logfiles")
+    println(readchomp(`SCHTASKS /DELETE /TN ExternalFMIExportTesting\\BouncingBall-FMPy /f`))    
     @test false
 end
-println(readchomp(`SCHTASKS /DELETE /TN ExternalFMIExportTesting\\BouncingBall-FMPy /f`))
+
 
 
 # @test length(solution_FMPy) == 1001
--- test/bouncing_ball/fmpy-bouncing_ball.py
+++ test/bouncing_ball/fmpy-bouncing_ball.py
@@ -1,41 +1,41 @@
-import argparse, sys, os, traceback
+import sys, os, traceback
 
-parser=argparse.ArgumentParser()
+with open(sys.argv[1]) as f:
+    lines = f.read().splitlines()
 
-parser.add_argument("--t_start", help="start-time for FMU simulation")
-parser.add_argument("--t_stop", help="stop-time for FMU simulation")
-parser.add_argument("--fmufile", help="path to .fmu file")
-parser.add_argument("--logfile", help="logfile for redirecting output")
-parser.add_argument("--lockfile", help="lockfile for indicating status")
-parser.add_argument("--juliatestflag", help="flag for printing to log that gets replaced by \"@test \" by julia afterwards")
+lockfile = lines[0]
+logfile = lines[1]
+fmufile = lines[2]
+juliatestflag = lines[3]
+t_start = float(lines[4])
+t_stop = float(lines[5])
 
-args=parser.parse_args()
-
-f = open(args.lockfile, "w+")
+f = open(lockfile, "w+")
 f.write("FMPy_running")
 f.close()
 
-with open(args.logfile, "w+") as sys.stdout:
+with open(logfile, "w+") as sys.stdout:
     print("fmpy-bouncing_ball.py log:")
     print("redirecting output...")
     import fmpy 
-    fmpy.dump(args.fmufile)
-    solution_FMPy = fmpy.simulate_fmu(filename=args.fmufile, validate=False, start_time=args.t_start, stop_time=args.t_stop, record_events=True, solver="Euler") # fmi_call_logger=lambda s: print('[FMI] ' + s)
-    #solution_FMPy = fmpy.simulate_fmu(filename=args.fmufile, validate=False, start_time=args.t_start, stop_time=args.t_stop, record_events=True, solver="CVode") # fmi_call_logger=lambda s: print('[FMI] ' + s)
+    fmpy.dump(fmufile)
+    solution_FMPy = fmpy.simulate_fmu(filename=fmufile, validate=False, start_time=t_start, stop_time=t_stop, record_events=True, solver="Euler") # fmi_call_logger=lambda s: print('[FMI] ' + s)
+    #solution_FMPy = fmpy.simulate_fmu(filename=fmufile, validate=False, start_time=t_start, stop_time=t_stop, record_events=True, solver="CVode") # fmi_call_logger=lambda s: print('[FMI] ' + s)
     try:
-        print(args.juliatestflag+"(length_solution_FMPy="+str(len(solution_FMPy))+") == 1001")
+        print(juliatestflag+"(length_solution_FMPy="+str(len(solution_FMPy))+") == 1001")
         # ts = collect(solution_FMPy[i][1] for i in 1:len(solution_FMPy))
         # ss = collect(solution_FMPy[i][2] for i in 1:len(solution_FMPy))
         # vs = collect(solution_FMPy[i][3] for i in 1:len(solution_FMPy))
-        print(args.juliatestflag+"isapprox((t_stop="+str(solution_FMPy[-1][0])+"), 5.0; atol=1e-6) # @test isapprox(ts[end], t_stop; atol=1e-6)")
-        print(args.juliatestflag+"isapprox((s_end="+str(solution_FMPy[-1][1])+"), 0.23272552; atol=1e-6) # @test isapprox(ss[end], 0.23272552; atol=1e-6)")
-        # print(args.juliatestflag+"isapprox((v_ens="+str(solution_FMPy[-1][2])+"), -0.17606235; atol=1e-6) # @test isapprox(vs[end], -0.17606235; atol=1e-6)")
+        print(juliatestflag+"isapprox((t_stop="+str(solution_FMPy[-1][0])+"), 5.0; atol=1e-6) # @test isapprox(ts[end], t_stop; atol=1e-6)")
+        print(juliatestflag+"isapprox((s_end="+str(solution_FMPy[-1][1])+"), 0.23272552; atol=1e-6) # @test isapprox(ss[end], 0.23272552; atol=1e-6)")
+        # print(juliatestflag+"isapprox((v_ens="+str(solution_FMPy[-1][2])+"), -0.17606235; atol=1e-6) # @test isapprox(vs[end], -0.17606235; atol=1e-6)")
     except Exception:
         print(traceback.format_exc())  
-        print(args.juliatestflag+"false # exception occured in python script")
+        print(juliatestflag+"false # exception occured in python script")
+    print("fmpy-bouncing_ball.py done")
         
     
-f = open(args.lockfile, "w")
+f = open(lockfile, "w")
 f.write("FMPy_done")
 f.close()
-os.remove(args.lockfile)
+os.remove(lockfile)
