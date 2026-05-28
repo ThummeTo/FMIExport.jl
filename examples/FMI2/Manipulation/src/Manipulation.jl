559c810ba5a80e6ac841bdb99c181eaf225df13a
ThummeTo <tobias.thummerer@uni-a.de>
2026-05-28 15:26:49 +0200

    fixed manipulation example

--- examples/FMI2/Manipulation/src/Manipulation.jl
+++ examples/FMI2/Manipulation/src/Manipulation.jl
@@ -84,15 +84,15 @@ fmu = FMIBUILD_CONSTRUCTOR(dirname(sourceFMU))
 # this is not required for export but a good idea anyway: 
 # the export takes a long time and exporting a possibly broken FMU does not help anyone
 
-# using FMI, DifferentialEquations
-# fmu.executionConfig.loggingOn = true
-# solution = simulateME(
-#     fmu,
-#     (0.0, 5.0);
-#     dtmax = 0.1,
-#     saveat = 0.0:0.01:5.0,
-#     recordValues = [fmi2ValueReference(335544320)],
-# )
+using FMI, DifferentialEquations
+fmu.executionConfig.loggingOn = true
+solution = simulateME(
+    fmu,
+    (0.0, 5.0);
+    dtmax = 0.1,
+    saveat = 0.0:0.01:5.0,
+    recordValues = [fmi2ValueReference(335544320)],
+)
 
 # using Plots
 # plot(solution)
